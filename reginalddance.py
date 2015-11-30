e33991ce30494d36a3c1633c1dd02bb396addb3b
Josh Kiblin <joshkiblin@gmail.com>
2015-11-30 10:19:10 -0500

    added file reginalddance.py

---
+++ reginalddance.py
@@ -0,0 +1,10 @@
+>>> reginald = Giraffes()
+>>> reginald.dance()
+left foot forward 
+left foot back 
+right foot forward 
+right foot back
+left foot back 
+right foot back 
+right foot forward 
+left foot forward 
